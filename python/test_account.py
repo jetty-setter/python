3bc265497d458283058d56c323d1d3cb561403c7
jetty-setter <123393626+jetty-setter@users.noreply.github.com>
2023-08-09 19:20:23 -0500

    Converted to Pytest and updated Withdraw test

--- python/test_account.py
+++ python/test_account.py
@@ -1,40 +1,39 @@
-import unittest
-from account import *
+from account import Account
+import pytest
 
 
-class TestAccount(unittest.TestCase):
+def test_init():
+    account1 = Account('Checking')
+    account2 = Account('Savings')
 
-    def setUp(self):
-        self.account1 = Account('Checking')
-        self.account2 = Account('Savings')
-
-    def tearDown(self):
-        del self.account1
-        del self.account2
-
-    def test_init(self):
-        self.assertEqual(self.account1.get_name(), 'Checking')
-        self.assertEqual(self.account1.get_balance(), 0)
-        self.assertEqual(self.account2.get_name(), 'Savings')
-        self.assertEqual(self.account2.get_balance(), 0)
-
-    def test_deposit(self):
-        assert self.account1.deposit(100.0) is True
-        self.assertEqual(self.account1.get_balance(), 100.0)
-        assert self.account2.deposit(0) is False
-        self.assertEqual(self.account2.get_balance(), 0)
-        assert self.account1.deposit(-25.0) is False
-        self.assertEqual(self.account1.get_balance(), 100.0)
-
-    def test_withdraw(self):
-        assert self.account1.deposit(100.0) is True
-        assert self.account1.withdraw(25.0) is True
-        self.assertEqual(self.account1.get_balance(), 75.0)
-        assert self.account1.withdraw(100.0) is False
-        self.assertEqual(self.account1.get_balance(), 75.0)
-        assert self.account2.withdraw(-100) is False
-        self.assertEqual(self.account2.get_balance(), 0)
+    assert account1.get_name() == 'Checking'
+    assert account1.get_balance() == pytest.approx(0)
+    assert account2.get_name() == 'Savings'
+    assert account2.get_balance() == pytest.approx(0)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_deposit():
+    account1 = Account('Checking')
+    account2 = Account('Savings')
+
+    assert account1.deposit(100.0) is True
+    assert account1.get_balance() == pytest.approx(100.0)
+    assert account2.deposit(0) is False
+    assert account2.get_balance() == pytest.approx(0)
+    assert account1.deposit(-25.0) is False
+    assert account1.get_balance() == pytest.approx(100.0)
+
+
+def test_withdraw():
+    account1 = Account('Checking')
+    account2 = Account('Savings')
+
+    account1.deposit(100.0)
+    assert account1.withdraw(25.0) is True
+    assert account1.get_balance() == pytest.approx(75.0)
+    assert account1.withdraw(100.0) is False
+    assert account1.get_balance() == pytest.approx(75.0)
+    assert account1.withdraw(75.0) is True
+    assert account1.get_balance() == pytest.approx(0)
+    assert account2.withdraw(-100) is False
+    assert account2.get_balance() == pytest.approx(0)
